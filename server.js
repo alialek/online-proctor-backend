8f9a735cfe59ba41b5a049ef7876e35942ee9dcd
Александр Алибутаев <alialek@yandex.ru>
2019-08-16 09:29:07 +0300

    'change'

--- server.js
+++ server.js
@@ -4,15 +4,6 @@ const connectDB = require('./config/db');
 const app = express();
 const path = require('path');
 
-// ... other app.use middleware
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-// ...
-// Right before your app.listen(), add this:
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
 connectDB();
 
 app.use(express.json({ extended: false }));
@@ -21,6 +12,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/quest', require('./routes/api/quest'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/orders', require('./routes/api/orders'));
+
+// Serve static assets in prod
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
