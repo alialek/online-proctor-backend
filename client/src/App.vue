09d4e6b0e401a9dd434b2251b97e7841fc62292c
Александр Алибутаев <alialek@yandex.ru>
2019-08-20 17:22:20 +0300

    'arFIX'

--- client/src/App.vue
+++ client/src/App.vue
@@ -21,6 +21,22 @@ export default {
     isLoading() {
       return this.$store.state.quest.isLoading;
     }
+  },
+
+  beforeCreate() {
+    var constraints = {
+      audio: false,
+      video: { facingMode: { exact: "environment" } }
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function(mediaStream) {
+        alert("success");
+      })
+      .catch(function(err) {
+        alert(err.name + ": " + err.message);
+      });
   }
 };
 </script>
